bad15e8ce45dd0497cc22e3b673bc52c74d26e93
Tony Ren <tonyren@Tonys-MacBook-Pro.local>
2021-12-09 17:53:56 -0500

    Finished implementing comparison page for basic features

--- Project/ViewCompare.swift
+++ Project/ViewCompare.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SwiftUI
 
 protocol QueryDelegate{
     func setCard(for index: CChar, card: Card)
@@ -32,8 +33,8 @@ class ViewCompare: UIViewController, QueryDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setSpecsHidden(to: true, for: 0)
-        setSpecsHidden(to: true, for: 1)
+        setSpecsHidden(to: true, index: 0)
+        setSpecsHidden(to: true, index: 1)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -45,7 +46,9 @@ class ViewCompare: UIViewController, QueryDelegate {
         }
     }
     
-    func setSpecsHidden(to val: Bool, for index: Int){
+    func setSpecsHidden(to val: Bool, index: Int){
+        self.carImage[index].image = nil
+        self.carImage[index].isHidden = val
         self.price[index].isHidden = val
         self.engine[index].isHidden = val
         self.horsepower[index].isHidden = val
@@ -61,7 +64,7 @@ class ViewCompare: UIViewController, QueryDelegate {
     func setCard(for index: CChar, card: Card){
         let indexInt = Int(index)
         self.card[indexInt] = card
-        setSpecsHidden(to: false, for: indexInt)
+        setSpecsHidden(to: false, index: indexInt)
         
         self.carName[indexInt].text = self.card[indexInt]?.getCarName()
         self.price[indexInt].text = self.card[indexInt]?.getPrice()
@@ -72,7 +75,92 @@ class ViewCompare: UIViewController, QueryDelegate {
         self.capacity[indexInt].text = self.card[indexInt]?.getFuelCap()
         self.sales[indexInt].text = self.card[indexInt]?.getSales()
         
-        self.carImage[indexInt].image = self.card[indexInt]?.carImg?.image
+        self.carImage[indexInt].load(url: (self.card[indexInt]?.getImgPath()) ?? " car")
+        
+        if (self.card[0] != nil && self.card[1] != nil) {
+            setAdvantages()
+        }
+    }
+    
+    func setAdvantages(){
+        let price1 = priceToFloat(price[0].text!)
+        let price2 = priceToFloat(price[1].text!)
+        if (price1 < price2) { setHighlight(for: price[0])}
+        else if (price2 < price1) { setHighlight(for: price[1])}
+        
+        let eng1 = Float(engine[0].text!)!
+        let eng2 = Float(engine[1].text!)!
+        if (eng1 > eng2) { setHighlight(for: engine[0])}
+        else if (eng2 > eng1) { setHighlight(for: engine[1])}
+        
+        let hp1 = Int(horsepower[0].text!)!
+        let hp2 = Int(horsepower[1].text!)!
+        if (hp1 > hp2) { setHighlight(for: horsepower[0])}
+        if (hp2 > hp1) { setHighlight(for: horsepower[1])}
+        
+        let wb1 = Float(wheelbase[0].text!)!
+        let wb2 = Float(wheelbase[1].text!)!
+        if (wb1 > wb2) { setHighlight(for: wheelbase[0])}
+        if (wb2 > wb1) { setHighlight(for: wheelbase[1])}
+        
+        let fe1 = effToInt(efficiency[0].text!)
+        let fe2 = effToInt(efficiency[1].text!)
+        if (fe1 > fe2) { setHighlight(for: efficiency[0])}
+        if (fe2 > fe1) { setHighlight(for: efficiency[1])}
+        
+        let fc1 = Float(capacity[0].text!)!
+        let fc2 = Float(capacity[1].text!)!
+        if (fc1 > fc2) { setHighlight(for: capacity[0])}
+        if (fc2 > fc1) { setHighlight(for: capacity[1])}
+
+        let sale1 = salesToInt(sales[0].text!)
+        let sale2 = salesToInt(sales[1].text!)
+        if (sale1 > sale2) { setHighlight(for: sales[0])}
+        if (sale2 > sale1) { setHighlight(for: sales[1])}
+    }
+    
+    func setHighlight(for label: UILabel){
+        label.textColor = .systemGreen
+        label.font = UIFont.boldSystemFont(ofSize: label.font.pointSize)
+    }
+    
+    func clearHighlights(){
+        for i in 0..<2 {
+            clearHighlight(for: price[i])
+            clearHighlight(for: engine[i])
+            clearHighlight(for: horsepower[i])
+            clearHighlight(for: wheelbase[i])
+            clearHighlight(for: efficiency[i])
+            clearHighlight(for: capacity[i])
+            clearHighlight(for: sales[i])
+        }
+    }
+    
+    private func clearHighlight(for label: UILabel){
+        label.textColor = .label
+        label.font = UIFont.systemFont(ofSize: label.font.pointSize)
+    }
+    
+    private func priceToFloat(_ el: String) -> Float{
+        var str = el
+        if let i = str.firstIndex(of: "$"){
+            str.remove(at: i)
+        }
+        
+        return Float(str)!
+    }
+    
+    private func effToInt(_ el: String) -> Int {
+        var str = el
+        if let i = str.firstIndex(of: " "){
+            str.removeSubrange(i...)
+        }
+        
+        return Int(str)!
+    }
+    
+    private func salesToInt(_ el: String) -> Int {
+        return effToInt(el)
     }
     
     
@@ -83,5 +171,19 @@ class ViewCompare: UIViewController, QueryDelegate {
     
     @IBAction func addCar2(_ sender: Any) {
         addIndex = 1
+    }
+    
+    @IBAction func removeCar1(_ sender: Any) {
+        clearHighlights()
+        setSpecsHidden(to: true, index: 0)
+        card[0] = nil
+        carName[0].text = "Car 1"
+    }
+    
+    @IBAction func removeCar2(_ sender: Any) {
+        clearHighlights()
+        setSpecsHidden(to: true, index: 1)
+        card[1] = nil
+        carName[1].text = "Car 2"
     }
 }
